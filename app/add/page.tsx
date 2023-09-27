b47939f5db8e918893d8a41dc559531c82c65fca
Yuya Inoue <yuyatuta@icloud.com>
2023-09-28 01:29:35 +0900

    必要なリンクを用意、移動可能に. サンプルデータで円グラフを描画可能に.

--- app/add/page.tsx
+++ app/add/page.tsx
@@ -1,9 +1,8 @@
 export default function Sample1() {
     return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1 className={`mb-3 text-2xl font-semibold`}>Hello World!</h1>
-        <p>This page is sample1 app!</p>
-      </main>
+      <div className={"add-main"}>
+        <h1>ADD Page</h1>
+      </div>
     )
   }
   
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+const mainClass = {}
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className={`mb-3 text-2xl font-semibold`}>Hello World!</h1>
-      <p>This page is sample!</p>
-    </main>
+    <div className="home-main">
+      <h1>Home</h1>
+    </div>
   )
 }
