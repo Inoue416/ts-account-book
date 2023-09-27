04cf3af0411d0906ec1742d4fe222f7d2f6d2462
Yuya Inoue <yuyatuta@icloud.com>
2023-09-27 23:49:19 +0900

    App Routerの挙動を確認

---
+++ app/sample1/page.tsx
@@ -0,0 +1,9 @@
+export default function Sample1() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1 className={`mb-3 text-2xl font-semibold`}>Hello World!</h1>
+        <p>This page is sample1 app!</p>
+      </main>
+    )
+  }
+  
