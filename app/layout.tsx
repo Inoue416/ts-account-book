f96fadfa2a9397ce843beabdb17b902356b9c23f
Yuya Inoue <yuyatuta@icloud.com>
2023-09-28 15:02:23 +0900

    footer追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,6 +46,15 @@ export default function RootLayout({
           </div>
         </nav>
         {children}
+
+        <footer className="m-4">
+          <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+            <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+            <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 YuyaInoue</span>
+          </div>
+        </footer>
+
+
       </body>
     </html>
   )
