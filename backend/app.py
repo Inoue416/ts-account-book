9aea728449e6fc4c6489328cb7b636a80fcd541f
Yuya Inoue <yuyatuta@icloud.com>
2023-09-29 15:05:13 +0900

    バックエンドのサンプルコードの起動確認

--- backend/app.py
+++ backend/app.py
@@ -1,2 +1,13 @@
 # バックエンド
 # 主にデータ管理
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/sample')
+def hello():
+    return "Hello from Flask!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
