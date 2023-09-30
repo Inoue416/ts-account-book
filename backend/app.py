b427884c98176b5b6bae6286e7095a0d5149554e
Yuya Inoue <yuyatuta@icloud.com>
2023-09-30 17:34:38 +0900

    月別のデータを取得するAPIの追加と取得までのテスト完了

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,7 @@ from flask import Flask, request
 from flask_cors import CORS
 import json
 from database import *
+from datetime import datetime
 
 
 app = Flask(__name__)
@@ -28,11 +29,33 @@ def insert_data(thing, money, kind):
 
     except:
         return 0
-    
+
+def request_month_data() -> list:
+    """
+        This function have not params.
+        return: [(id, money, thing, kind, created_at, update_at), (..), ...]
+    """
+    conn = create_connect(DBNAME)
+    cur = get_cursor(conn)
+    now_ym = datetime.now().strftime("%Y-%m")
+    cmd = """
+        SELECT * FROM posts WHERE strftime("%Y-%m", updated_at) = ?;
+    """
+    month_data = cur.execute(cmd, [now_ym,]).fetchall()
+    close_db(conn)
+    return month_data
 
 @app.route('/send_sample_message')
 def hello():
     return "Hello from Flask!"
+
+@app.route('/get_month_data', methods=["GET"])
+def get_month_data():
+    res = "error"
+    if request.method == "GET":
+        request_month_data()
+        res = "success"
+    return {"response_message": res}
 
 @app.route("/add_info", methods=["POST"])
 def add_info():
