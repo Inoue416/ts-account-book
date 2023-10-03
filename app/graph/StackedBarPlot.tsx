549279fdb4b286a4dd0763ac0361c740f174a512
Yuya Inoue <yuyatuta@icloud.com>
2023-10-03 19:33:41 +0900

    棒グラフのデータの取得と描画機能の完成

--- app/graph/StackedBarPlot.tsx
+++ app/graph/StackedBarPlot.tsx
@@ -1,6 +1,7 @@
 // TODO: ダミーデータ部分を状態管理に移行する
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -39,29 +40,71 @@ export const options = {
   },
 };
 
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: 'income',
-      data: [100, 200, 300],
-      backgroundColor: 'rgb(255, 99, 132)',
-    },
-    {
-      label: 'Item 1',
-      data: [-1000, -80, -50],
-      backgroundColor: 'rgb(75, 192, 192)',
-    },
-    {
-      label: 'Item 2',
-      data: [-200, -20, -1050],
-      backgroundColor: 'rgb(53, 162, 235)',
-    },
-  ],
-};
+const labels = [
+    "January",   
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
 
 export function StackedBarPlot() {
-  return <Bar options={options} data={data} />;
+    const [year_data, setYearData] = useState({
+        labels: labels,
+        datasets: []
+    })
+
+    let data = {
+      labels,
+      datasets: [
+        {
+          label: 'Income',
+          data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          backgroundColor: 'rgb(75, 192, 192)',
+        },
+        {
+          label: 'Payment',
+          data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          backgroundColor: 'rgb(255, 99, 132)',
+        }
+      ],
+    }
+
+    useEffect(() => {
+        axios.get(
+            'http://127.0.0.1:5000/get_year_data'
+        ).then((response) => {
+            let res_data = response.data
+            console.log(res_data.response_message)
+            const year_datas = res_data.data
+            Object.keys(year_datas).map((key, index) => {
+              year_datas[key].forEach((value, arr_index, arr) => {
+                let month: Number = Number(value[5].split("-", 2)[1]) - 1
+                console.log(value[3])
+                if (value[3] === "income") {
+                  console.log('I')
+                  data.datasets[0].data[month] += Number(value[1])
+                } else {
+                  console.log("P")
+                  data.datasets[1].data[month] += Number(value[1])
+                }
+              })
+            })
+            setYearData(data)
+        }).catch((error) => {
+            console.log(error)
+        })
+        }, [])
+    return (
+    <div className="bar-plot-area">
+        <Bar options={options} data={year_data} />
+    </div>
+  );
 }
